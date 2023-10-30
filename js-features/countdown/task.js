db5668b09f5116e8905fb1e4e7922533033e5d1e
ZavyalovAndrei <zaver1983@gmail.com>
2023-10-30 03:17:15 +0300

    first commit countdown

---
+++ js-features/countdown/task.js
@@ -0,0 +1,28 @@
+const timeToSeconds = function() {
+    const startTime = document.getElementById("timer");
+    const fullTimer = startTime.textContent.split(":")
+    if (fullTimer.length < 3) {
+        startTime.textContent = timerCheck(startTime.textContent);
+    } else {
+        const fullTimerInSeconds = (fullTimer[0] * 3600) + (fullTimer[1] * 60) + ( + fullTimer[2]);
+        startTime.textContent = timerCheck(fullTimerInSeconds);
+    }
+}
+
+const timerCheck = function(timerSeconds){
+    if (+timerSeconds === 0) {
+        clearInterval(timer);
+        window.location.href = "https://inkscape.org/gallery/item/42333/inkscape-1.3_2023-07-21_0e150ed6c4-x64_31XBEKV.msi";
+        alert("Вы победили в конкурсе!");
+    } else{  
+        timerSeconds--;
+        const hours = Math.trunc(timerSeconds / 3600);
+        const minutes = Math.trunc((timerSeconds - (hours * 3600)) / 60);
+        const seconds = timerSeconds - (hours * 3600) - (minutes * 60);
+        return hours.toString().padStart(2, '0')
+        + ':' + minutes.toString().padStart(2, '0')
+        + ':' + seconds.toString().padStart(2, '0');
+    }
+}
+
+let timer = setInterval(timeToSeconds , 1000);
