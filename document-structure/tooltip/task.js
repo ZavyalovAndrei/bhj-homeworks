f427be4bdd3e020a22c3949c688ed5f0e5336b00
ZavyalovAndrei <zaver1983@google.com>
2023-11-29 21:54:54 +0100

    Fix 1 tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -2,61 +2,74 @@ document.addEventListener("click", (event) => {
   event.preventDefault();
   const classArray = [...event.target.classList];
   if (classArray.includes("has-tooltip")) {
-    hideTooltips();
     const tooltipText = event.target.getAttribute("title");
-    const textParameters = event.target.getBoundingClientRect();
-    event.target.insertAdjacentHTML(
-      "afterend",
-      '<div class="tooltip tooltip_active" style="left: 0; top: 0">' +
-        tooltipText +
-        "</div>"
-    );
-    const tooltipElement = document.querySelector(".tooltip_active");
-    const tooltipParameters = tooltipElement.getBoundingClientRect();
-    let position = "bottom";
-    const windowWidth = document.documentElement.clientWidth;
-    const windowHeight = document.documentElement.clientHeight;
-    let xPosition =
-      textParameters.left +
-      0.5 * textParameters.width -
-      0.5 * tooltipParameters.width;
-    let yPosition = textParameters.bottom + 5;
-    const rightPositionCheck = xPosition + 5;
-    const lefttPositionCheck =
-      windowWidth - xPosition - tooltipParameters.width + 5;
-    const topPositionCheck =
-      windowHeight - yPosition - tooltipParameters.height;
-    if (rightPositionCheck < 0) {
-      position = "right";
-      xPosition = textParameters.right + 5;
-      yPosition =
-        textParameters.top +
-        0.5 * textParameters.height -
-        0.5 * tooltipParameters.height;
-    } else if (lefttPositionCheck < 0) {
-      position = "left";
-      xPosition = textParameters.left - tooltipParameters.width - 5;
-      yPosition =
-        textParameters.top +
-        0.5 * textParameters.height -
-        0.5 * tooltipParameters.height;
-    } else if (topPositionCheck < 0) {
-      position = "top";
-      yPosition = textParameters.top - tooltipParameters.height - 5;
-    }
-
-    tooltipElement.setAttribute(
-      "style",
-      "left: " + xPosition + "px; top: " + yPosition + "px"
-    );
-    tooltipElement.dataset.position = position;
-    console.log(tooltipElement);
+    const currentTooltip = checkTooltips(tooltipText);
+    if (currentTooltip === null) {
+      event.target.insertAdjacentHTML(
+        "afterend",
+        '<div class="tooltip tooltip_active" style="left: 0; top: 0">' +
+          tooltipText +
+          "</div>"
+      );
+      const tooltipElement = document.querySelector(".tooltip_active");
+      tooltipElement.dataset.position = changePosition(tooltipElement, event.target);
+    } else {
+        currentTooltip.classList.toggle("tooltip_active");
+        currentTooltip.dataset.position = changePosition(currentTooltip, event.target);
+      }
   }
 });
 
-function hideTooltips() {
+function checkTooltips(currentText) {
+  let result = null;
   const tooltips = document.querySelectorAll(".tooltip");
   tooltips.forEach((tooltip) => {
-    tooltip.classList.remove("tooltip_active");
+    if (tooltip.textContent === currentText) {
+      result =  tooltip;
+    } else {
+      tooltip.classList.remove("tooltip_active");
+    }
   });
+  return result;
+}
+
+function changePosition(element, linkElement) {
+  const tooltipParameters = element.getBoundingClientRect();
+  const textParameters = linkElement.getBoundingClientRect();
+  let position = "bottom";
+  const windowWidth = document.documentElement.clientWidth;
+  const windowHeight = document.documentElement.clientHeight;
+  let xPosition =
+    textParameters.left +
+    0.5 * textParameters.width -
+    0.5 * tooltipParameters.width;
+  let yPosition = textParameters.bottom + 5;
+  const rightPositionCheck = xPosition + 5;
+  const lefttPositionCheck =
+    windowWidth - xPosition - tooltipParameters.width + 5;
+  const topPositionCheck = windowHeight - yPosition - tooltipParameters.height;
+  if (rightPositionCheck < 0) {
+    position = "right";
+    xPosition = textParameters.right + 5;
+    yPosition =
+      textParameters.top +
+      0.5 * textParameters.height -
+      0.5 * tooltipParameters.height;
+  } else if (lefttPositionCheck < 0) {
+    position = "left";
+    xPosition = textParameters.left - tooltipParameters.width - 5;
+    yPosition =
+      textParameters.top +
+      0.5 * textParameters.height -
+      0.5 * tooltipParameters.height;
+  } else if (topPositionCheck < 0) {
+    position = "top";
+    yPosition = textParameters.top - tooltipParameters.height - 5;
+  }
+
+  element.setAttribute(
+    "style",
+    "left: " + xPosition + "px; top: " + yPosition + "px"
+  );
+  return position;
 }
